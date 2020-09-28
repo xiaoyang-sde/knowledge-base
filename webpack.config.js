e88ce14ad6a92b2dbde4d5220205533259c0659c
Xiaoyang Liu <siujoeng.lau@gmail.com>
2020-09-28 10:41:41 +0800

    Setup npm scripts, webpack, eslint, babel (#8)

---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const webpack = require('webpack');
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const fileExtensions = ['jpg', 'jpeg', 'png', 'gif', 'eot', 'otf', 'svg', 'ttf', 'woff', 'woff2'];
+
+const options = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: {
+    popup: path.join(__dirname, 'src', 'js', 'popup.js'),
+    options: path.join(__dirname, 'src', 'js', 'options.js'),
+    background: path.join(__dirname, 'src', 'js', 'background.js'),
+    'content-script': path.join(__dirname, 'src', 'js', 'content-script.js'),
+  },
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: new RegExp(`.(${fileExtensions.join('|')})$`),
+        loader: 'file-loader?name=[name].[ext]',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.EnvironmentPlugin(['NODE_ENV']),
+    new CopyWebpackPlugin({
+      patterns: [{
+        from: 'src/manifest.json',
+        to: 'build/',
+        transform(content) {
+          return Buffer.from(JSON.stringify({
+            description: process.env.npm_package_description,
+            version: process.env.npm_package_version,
+            ...JSON.parse(content.toString()),
+          }));
+        },
+      }],
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'html', 'popup.html'),
+      filename: 'popup.html',
+      chunks: ['popup'],
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'html', 'options.html'),
+      filename: 'options.html',
+      chunks: ['options'],
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'html', 'background.html'),
+      filename: 'background.html',
+      chunks: ['background'],
+    }),
+  ],
+};
+
+module.exports = options;
