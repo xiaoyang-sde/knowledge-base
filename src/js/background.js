a9e88ee8baf783bd893f6cd2fad4e38ed81094dc
Markliniubility <limarksmail@gmail.com>
2020-09-29 22:40:32 -0400

    New pop up; some attempt on auto fill
    auto fill is not working. I am not sure if the issue is the auto fill part or the auto-save part……
    
    Add an icon on context menu, which for now does nothing

--- src/js/background.js
+++ src/js/background.js
@@ -1,9 +1,18 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color: '#3aa757' }, () => {
-    console.log('The color is green.');
-  });
+//Right click to trigger auto-fill in context menu
+
+//put the option into the context menu
+var contextMenuItem = {
+  "id": "jobAutoFill",
+  "title": "Fill Job Application",
+  "contexts": ["all"]
+};
+chrome.contextMenus.create(contextMenuItem);
+
+//implement the onclick event on the context menu
+chrome.contextMenus.onClicked.addListener(function(clickData) {
+  if (clickData.menuItemId == "jobAutoFill") {
+    alert("Testing the Onclicked Event");
+  }
+    
 });
--- src/js/content-scripts.js
+++ src/js/content-scripts.js
@@ -1,1 +1,39 @@
-console.log('The is just a test');
+console.log("content script run!");
+
+
+//get the information from user input
+const allInput = document.getElementsByTagName('input');
+console.log(allInput[3].value);
+for (i = 0; i < allInput.length; i++) {
+    //First Name
+    if (allInput[i].autocomplete == "given-name") {
+        chrome.storage.sync.get('firstName', function(data) {
+            allInput[i].value = data.value;
+        });
+    }
+    //Last Name
+    if (allInput[i].autocomplete == "family-name") {
+        chrome.storage.sync.get('lastName', function(data) {
+            allInput[i].value = data.value;
+        });
+    }
+    //Email
+    if (allInput[i].autocomplete == "email") {
+        chrome.storage.sync.get('email', function(data) {
+            allInput[i].value = data.value;
+        });
+    }
+    //address line
+    if (allInput[i].autocomplete == "street-address") {
+        chrome.storage.sync.get('address', function(data) {
+            allInput[i].value = data.value;
+        });
+    }
+    //zip-code
+    if (allInput[i].autocomplete == "postal-code") {
+        chrome.storage.sync.get('zip', function(data) {
+            allInput[i].value = data.value;
+        });
+    }
+}
+
