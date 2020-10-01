8b82658843385dde103cd84873a9509b70e7c532
Markliniubility <limarksmail@gmail.com>
2020-09-30 23:11:30 -0400

    Update content-scripts.js

--- src/js/content-scripts.js
+++ src/js/content-scripts.js
@@ -8,31 +8,31 @@ for (i = 0; i < allInput.length; i++) {
     //First Name
     if (allInput[i].autocomplete == "given-name") {
         chrome.storage.sync.get('firstName', function(data) {
-            allInput[i].value = data.value;
+            this.value = data.value;
         });
     }
     //Last Name
     if (allInput[i].autocomplete == "family-name") {
         chrome.storage.sync.get('lastName', function(data) {
-            allInput[i].value = data.value;
+            this.value = data.value;
         });
     }
     //Email
     if (allInput[i].autocomplete == "email") {
         chrome.storage.sync.get('email', function(data) {
-            allInput[i].value = data.value;
+            this.value = data.value;
         });
     }
     //address line
     if (allInput[i].autocomplete == "street-address") {
         chrome.storage.sync.get('address', function(data) {
-            allInput[i].value = data.value;
+            this.value = data.value;
         });
     }
     //zip-code
     if (allInput[i].autocomplete == "postal-code") {
         chrome.storage.sync.get('zip', function(data) {
-            allInput[i].value = data.value;
+            this.value = data.value;
         });
     }
 }
