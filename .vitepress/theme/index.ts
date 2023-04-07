5fed778e4bd3d9e42201d0e295b568e94ced2fe7
Xiaoyang Liu <siujoeng.lau@gmail.com>
2023-04-06 23:18:55 -0700

    Remove unused dependencies
    Signed-off-by: Xiaoyang Liu <siujoeng.lau@gmail.com>

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,8 +1,4 @@
 import DefaultTheme from "vitepress/theme";
-import "@fontsource/source-serif-pro/200.css";
-import "@fontsource/source-serif-pro/400.css";
-import "@fontsource/source-serif-pro/600.css";
-import "@fontsource/source-serif-pro/700.css";
 import "./custom.css";
 
 export default DefaultTheme;
